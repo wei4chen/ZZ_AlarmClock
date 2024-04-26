8877830931103e32ebbdd40959e1ffcb53047fa8
wei4chen <thorwei214@gmail.com>
2024-04-26 14:37:00 +0800

    v1.06

--- app/src/main/java/com/thorwei/zz_alarmclock/BootReceiver.java
+++ app/src/main/java/com/thorwei/zz_alarmclock/BootReceiver.java
@@ -1,16 +1,17 @@
 package com.thorwei.zz_alarmclock;
 
+import static com.thorwei.zz_alarmclock.MainActivity.TAG;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
-    public BootReceiver() {
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+        if (intent.getExtras().get("title").equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Log.e(TAG,"onReceive");
             context.startService(new Intent(context, AlarmClockService.class));
         }
     }
